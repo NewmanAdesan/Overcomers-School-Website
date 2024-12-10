784c3e086dcf928f17ecaee48bc407c1bd3d295a
NewmanAdesan <newmanadesan@gmail.com>
2024-12-10 18:21:03 +0100

    FEATURE: implementation of the report viewing feature

--- src/components/homepage/section-2B/Section2B.tsx
+++ src/components/homepage/section-2B/Section2B.tsx
@@ -5,15 +5,17 @@ const Section2B = () => {
     <section className="mb-12 mt-3 sm:mt-0 md:-mt-6 mycontainer1 rounded-[10px] flex items-center justify-center relative overflow-hidden">
             <div className="my-14 flex flex-col items-center justify-center text-center gap-3">
                 <h2 className="poppins text-2xl sm:text-3xl md:text-48-auto font-semibold text-white">Online Result Checker</h2>
-                <button className="flex items-center gap-2 py-5 px-10 sm:px-14 bg-white rounded-32px shadow-[0px_2px_4px_rgba(40,41,61,0.04),_0px_8px_16px_rgba(96,97,112,0.16)] font-semibold">
+                <a 
+                  href="https://reportcard.shkula.com?referrer=https://overcomers-school-website.vercel.app"
+                  className="flex items-center gap-2 py-5 px-10 sm:px-14 bg-white rounded-32px shadow-[0px_2px_4px_rgba(40,41,61,0.04),_0px_8px_16px_rgba(96,97,112,0.16)] font-semibold">
                     <span className="text-shkula-primary text-xs md:text-sm">
-                        View Your 
+                        View Your
                         Child’s {' '}
                         <span className="hidden sm:inline">/ Children’s</span> {' '}
                         Result
                     </span>
                     <ArrowRight size={14} color="#4044A7" />
-                </button>
+                </a>
             </div>
             <figure className="absolute inset-0 -z-10  mx-4 md:mx-8 rounded-[10px] overflow-hidden">
                 <img src="/section-2-b.webp" alt="" className="size-full object-center object-cover" />
